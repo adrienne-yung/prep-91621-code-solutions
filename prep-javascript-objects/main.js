8ab347280de51f1edab7c37ddcd079c29913c65a
Adrienne Yung <adrienneyung64@gmail.com>
2021-09-27 19:18:36 -0700

    Completed prep-javascript-objects exercise.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,9 @@
+var person = { firstName: 'Adrienne', lastName: 'Yung', hobby: 'Gaming' };
+console.log(person);
+var fullName = person.firstName + ' ' + person.lastName;
+console.log("The person's name is:", fullName);
+person.job = 'student';
+console.log("The person's job is:", person.job);
+person.previousJob = 'Franchise trainer and Supervisor for Zero Degrees';
+console.log("The person's previous job is:", person.previousJob);
+console.log(person);
